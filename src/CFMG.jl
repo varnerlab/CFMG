b05edad24cd6f0339da0f0740cadb80a20bafa40
Zhiping Zhang <8401358+ZZP12@users.noreply.github.com>
2020-09-24 16:40:30 -0400

    copy JuCFMG.jl to CFMG.jl

--- src/CFMG.jl
+++ src/CFMG.jl
@@ -1,5 +1,20 @@
 module CFMG
 
-greet() = print("Hello World!")
+# includes -
+include("Include.jl")
+
+# methods to make various types of models -
+export make_julia_model
+export make_octave_model
+export make_python_model
+export make_matlab_model
+export make_vff_model
+export make_sbml_model
+
+# method to make a default project structure the user can edit -
+export generate_default_project
+
+# method to export MAT files to an editable format -
+export extract_files_from_cobra_model
 
 end # module
