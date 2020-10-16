f3b02aa8edc30e611133b5fcc3f13c7ee3cec26d
Zhiping Zhang <8401358+ZZP12@users.noreply.github.com>
2020-10-16 14:38:45 -0400

    update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,20 @@
 using CFMG
 using Test
 
+include("test1.jl")
+
 # write a default test -
 function default_cfmg_test()
-    return true 
+    return true
 end
 
 # setup the test set for the code generator -
 @testset "cfmg_test_set" begin
     @test default_cfmg_test() == true
+    @test CFMG_test1() == true
 end
+
+
+# @testset "example_model" begin
+#     @test CFMG_test1() == true
+# end
