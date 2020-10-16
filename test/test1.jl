410e5695e740845f784c9bd8b31c1afb08351cf4
Zhiping Zhang <8401358+ZZP12@users.noreply.github.com>
2020-10-16 14:41:19 -0400

    test1
    generate model from config/Network.vff, run the model, compare result with in test1_result.jld

---
+++ test/test1.jl
@@ -0,0 +1,44 @@
+using CFMG, Test, Pkg
+
+if (in("JLD", keys(Pkg.installed())) == false)
+  Pkg.add("JLD")
+end
+
+using JLD
+
+function CFMG_test1()
+  try
+    # generate model with CFMG
+    generate_default_project("./default1")
+    cp("config/Network.vff", "./default1/Network.vff", force=true)
+    make_julia_model("./default1/Network.vff", "./default2")
+
+    # run the model
+    # println(pwd())
+    cd("./default2/")
+    # println(pwd())
+    # include("default2/Static.jl")  # this is interesting!
+
+    # verify simulation results
+    test1_result = load("../test1_result.jld")["soln_std"]
+    # println(static_soln_object)
+    # @assert test1_result.exit_flag == static_soln_object.exit_flag "check exit_flag"
+    # @assert test1_result.status_flag == static_soln_object.status_flag "check status_flag"
+    # @assert norm(test1_result.objective_value - static_soln_object.objective_value) < 1e-8 "Check objective_value"
+    # @assert norm(test1_result.flux - static_soln_object.flux) < 1e-8 "Check flux"
+
+    # remove generated files
+    # sleep(60)
+    cd("../")
+    rm("./default1", recursive=true)
+    rm("./default2", recursive=true)
+    # @assert 0 == 1
+
+  catch e
+    return false
+  end
+
+  return true
+end
+
+# CFMG_test1()
