96d358a4fcd7c105a25638de40e721439d951c75
Zhiping Zhang <8401358+ZZP12@users.noreply.github.com>
2020-10-24 02:46:22 -0400

    update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,13 +3,23 @@ using CFMG
 
 makedocs(
     sitename = "CFMG",
-    format = Documenter.HTML(),
-    modules = [CFMG]
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true"
+    ),
+    modules = [CFMG],
+    pages = [
+    "Home" => "index.md",
+    "installation.md",
+    "Examples" => Any[
+        "Example 1" => "examples/example1.md",
+        ]
+    ],
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/varnerlab/CFMG.git"
+    repo = "github.com/varnerlab/CFMG.git",
+    devurl = "stable",
 )
